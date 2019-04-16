a59361a405b397815381d8b341fc88722db3360a
Steve Mulligan <steve@nonlocal.ca>
2019-04-16 17:23:39 -0400

    add enpoints that will turn off after N seconds

--- src/index.js
+++ src/index.js
@@ -18,10 +18,27 @@ app.get('/at/:id/:r/:g/:b', async function(req, res){
 	res.send('ok');
 });
 
+app.get('/at/:id/:r/:g/:b:/:timeout', async function(req, res){
+	await focus.command("led.at", req.params.id, req.params.r, req.params.g, req.params.b);
+	res.send('ok');
+	setTimeout(async function() {
+		await focus.command("led.at", req.params.id, 0, 0, 0);
+	}, req.params.timeout * 1000)
+});
+
 app.get('/all/:r/:g/:b', async function(req, res){
 	await focus.command("led.setAll", req.params.r, req.params.g, req.params.b);
 	res.send('ok');
 });
+
+app.get('/all/:r/:g/:b/:timeout', async function(req, res){
+	await focus.command("led.setAll", req.params.r, req.params.g, req.params.b);
+	res.send('ok');
+	setTimeout(async function() {
+		await focus.command("led.setAll", req.params.id, 0, 0, 0);
+	}, req.params.timeout * 1000)
+});
+
 
 
 app.listen(7890);
